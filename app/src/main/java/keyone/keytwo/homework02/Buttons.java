87c282cd08fff9ad49f0fb746b7f4f012e0520de
MEA <mayer_evgenija@mail.ru>
2021-07-04 01:51:31 +0700

    Добавила обработку остальных кнопок

--- app/src/main/java/keyone/keytwo/homework02/Buttons.java
+++ app/src/main/java/keyone/keytwo/homework02/Buttons.java
@@ -1,14 +1,49 @@
 package keyone.keytwo.homework02;
 
+import android.widget.Button;
+
 public class Buttons {
     private String buttonC;
     private String buttonEraseToTheLeft;
     private String buttonDivision;
+    private int button1;
+    private int button2;
+    private int button3;
+    private String buttonMultiply;
+    private int button4;
+    private int button5;
+    private int button6;
+    private String buttonMinus;
+    private int button7;
+    private int button8;
+    private int button9;
+    private String buttonPlus;
+    private String buttonPercent;
+    private int button0;
+    private String buttonComma;
+    private String buttonEqual;
 
     public Buttons() {
         buttonC = "C";
         buttonEraseToTheLeft = "Стереть символ слева";
         buttonDivision = "Деление";
+        button1 = 1;
+        button2 = 2;
+        button3 = 3;
+        buttonMultiply = "Умножение";
+        button4 = 4;
+        button5 = 5;
+        button6 = 6;
+        buttonMinus = "Минус";
+        button7 = 7;
+        button8 = 8;
+        button9 = 9;
+        buttonPlus = "Плюс";
+        buttonPercent = "Проценты";
+        button0 = 0;
+        buttonComma = "Запятая";
+        buttonEqual = "Равно";
+
     }
 
     public String getButtonC() {
@@ -21,5 +56,69 @@ public class Buttons {
 
     public String getButtonDivision() {
         return buttonDivision;
+    }
+
+    public int getButton1() {
+        return button1;
+    }
+
+    public int getButton2() {
+        return button2;
+    }
+
+    public int getButton3() {
+        return button3;
+    }
+
+    public String getButtonMultiply() {
+        return buttonMultiply;
+    }
+
+    public int getButton4() {
+        return button4;
+    }
+
+    public int getButton5() {
+        return button5;
+    }
+
+    public int getButton6() {
+        return button6;
+    }
+
+    public String getButtonMinus() {
+        return buttonMinus;
+    }
+
+    public int getButton7() {
+        return button7;
+    }
+
+    public int getButton8() {
+        return button8;
+    }
+
+    public int getButton9() {
+        return button9;
+    }
+
+    public String getButtonPlus() {
+        return buttonPlus;
+    }
+
+    public String getButtonPercent() {
+        return buttonPercent;
+    }
+
+    public int getButton0() {
+        return button0;
+    }
+
+    public String getButtonComma() {
+        return buttonComma;
+    }
+
+    public String getButtonEqual() {
+        return buttonEqual;
     }
 }
--- app/src/main/java/keyone/keytwo/homework02/MainActivity.java
+++ app/src/main/java/keyone/keytwo/homework02/MainActivity.java
@@ -17,10 +17,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button buttonC;
     private Button buttonEraseToTheLeft;
     private Button buttonDivision;
-
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button buttonMultiply;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button buttonMinus;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button buttonPlus;
+    private Button buttonPercent;
+    private Button button0;
+    private Button buttonComma;
+    private Button buttonEqual;
 
     private Buttons buttons;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,33 +54,111 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonC = findViewById(R.id.buttonC);
         buttonEraseToTheLeft = findViewById(R.id.buttonEraseToTheLeft);
         buttonDivision = findViewById(R.id.buttonDivision);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        buttonMultiply = findViewById(R.id.buttonMultiply);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        buttonMinus = findViewById(R.id.buttonMinus);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonPercent = findViewById(R.id.buttonPercent);
+        button0 = findViewById(R.id.button0);
+        buttonComma = findViewById(R.id.buttonComma);
+        buttonEqual = findViewById(R.id.buttonEqual);
+
         buttonC.setOnClickListener(this);
         buttonEraseToTheLeft.setOnClickListener(this);
         buttonDivision.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        buttonMultiply.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        buttonMinus.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonPlus.setOnClickListener(this);
+        buttonPercent.setOnClickListener(this);
+        button0.setOnClickListener(this);
+        buttonComma.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonC:
-                textView1.setText(String.format(Locale.getDefault(), "%s", buttons.getButtonC()));
+                textView1.setText(buttons.getButtonC());
                 break;
             case R.id.buttonEraseToTheLeft:
-                textView1.setText(String.format(Locale.getDefault(), "%s", buttons.getButtonEraseToTheLeft()));
+                textView1.setText(buttons.getButtonEraseToTheLeft());
                 break;
             case R.id.buttonDivision:
-                textView1.setText(String.format(Locale.getDefault(), "%s", buttons.getButtonDivision()));
+                textView1.setText(buttons.getButtonDivision());
                 break;
-
+            case R.id.button1:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton1()));
+                break;
+            case R.id.button2:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton2()));
+                break;
+            case R.id.button3:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton3()));
+                break;
+            case R.id.buttonMultiply:
+                textView1.setText(buttons.getButtonMultiply());
+                break;
+            case R.id.button4:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton4()));
+                break;
+            case R.id.button5:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton5()));
+                break;
+            case R.id.button6:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton6()));
+                break;
+            case R.id.buttonMinus:
+                textView1.setText(buttons.getButtonMinus());
+                break;
+            case R.id.button7:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton7()));
+                break;
+            case R.id.button8:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton8()));
+                break;
+            case R.id.button9:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton9()));
+                break;
+            case R.id.buttonPlus:
+                textView1.setText(buttons.getButtonPlus());
+                break;
+            case R.id.buttonPercent:
+                textView1.setText(buttons.getButtonPercent());
+                break;
+            case R.id.button0:
+                textView1.setText(String.format(Locale.getDefault(), "%d", buttons.getButton0()));
+                break;
+            case R.id.buttonComma:
+                textView1.setText(buttons.getButtonComma());
+                break;
+            case R.id.buttonEqual:
+                textView1.setText(buttons.getButtonEqual());
+                break;
         }
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         makeToast("onStart");
-
     }
 
     @Override
